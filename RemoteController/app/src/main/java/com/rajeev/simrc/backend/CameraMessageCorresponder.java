563d95ce247e4391e1a0a1f7bc6c58a04f08a9fc
rajeev <jeev2345@gmail.com>
2015-03-17 01:18:56 -0500

    test commit

---
+++ RemoteController/app/src/main/java/com/rajeev/simrc/backend/CameraMessageCorresponder.java
@@ -0,0 +1,7 @@
+package com.rajeev.simrc.backend;
+
+/**
+ * Created by rajeev on 3/15/15.
+ */
+public class CameraMessageCorresponder {
+}
---
+++ SimRC/app/src/main/java/com/rajeev/simrc/backend/Globals.java
@@ -0,0 +1,7 @@
+package com.rajeev.simrc.backend;
+
+/**
+ * Created by rajeev on 3/11/15.
+ */
+public class Globals {
+}
